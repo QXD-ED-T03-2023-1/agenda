3c967bfef25fa8ab5ffe00474dae4e649f0eab0a
Arthur Araruna <ararunaufc@gmail.com>
2023-03-14 17:30:52 -0300

    Inserindo contatos e verificando.
    - `push_back` é um método que adiciona um novo elemento no `vector` e o
    associa ao primeiro índice disponível, de maneira equivalente aos vetores
    em C.
    - `vector` permite que acessemos os elementos usando a mesma sintaxe que
    já conhecemos de indexação no C.
    - Também podemos usar `printf` em C++, porém a forma nativa de se
    imprimir no terminal é usando `cout`, vindo da biblioteca `<iostream>`.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <string>
 
@@ -11,9 +12,19 @@ struct Contato {
 int main() {
     vector<Contato> agenda;
     Contato inicial;
+    Contato segundo;
+
+    segundo.nome = "Cicranim dos Santos";
+    segundo.numero = "88888888";
 
     inicial.nome = "Fulanim da Silva";
     inicial.numero = "999999999";
+
+    agenda.push_back(inicial);
+    agenda.push_back(segundo);
+
+    cout << agenda[0].nome << "\n" << agenda[0].numero << "\n";
+    cout << agenda[1].nome << "\n" << agenda[1].numero << "\n";
 
     return 0;
 }
